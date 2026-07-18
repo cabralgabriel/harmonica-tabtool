19f4bd93503570bc11fd00e4b862d56a8d4e475f
cabralgabriel <gabrielcabralv@hotmail.com>
2026-07-18 18:10:55 -0300

    quick fix for score view

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+import os
 import sys
 from PySide6.QtWidgets import QApplication
 
 from gui.window import MainWindow
 
 if __name__ == "__main__":
+
+    os.environ["LC_NUMERIC"] = "C"
+    
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
